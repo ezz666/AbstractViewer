a51fbcd9635e3dc9b04b844280aa7107fafb87d4
Sergey <khilkov.s@gmail.com>
2018-07-02 15:48:33 -0400

    Adding file to simplify python2 and Python3 modules creation

---
+++ 2and3.h
@@ -0,0 +1,33 @@
+# ifndef TWOANDTHREE_H
+# define TWOANDTHREE_H
+#include "Python.h"
+#include "patchlevel.h"
+
+# if PYMAJOR_VERSION==2
+PyObject* MyPyBytes_FromStringAndSize(const char *v, Py_ssize_t len){
+  return PyString_FromStringAndSize(v, len);
+}
+int MyPyBytes_Check(PyObject *o){
+  return PyString_Check(o);
+}
+char* MyPyString_AsString(PyObject *string){
+  return PyString_AsString(string)
+}
+PyObject* MyBytes_FromUnicode(PyObject * string){
+  return string;
+}
+# elif PY_MAJOR_VERSION==3
+PyObject* MyPyBytes_FromStringAndSize(const char *v, Py_ssize_t len){
+  return PyBytes_FromStringAndSize(v, len);
+}
+int MyPyBytes_Check(PyObject *o){
+  return PyBytes_Check(o);
+}
+char* MyPyString_AsString(PyObject *string){
+  return PyBytes_AsString(string);
+}
+PyObject* MyBytes_FromUnicode(PyObject * string){
+  return PyUnicode_AsUTF8String(string);
+}
+# endif //PY_MAJOR_VERSION
+# endif // TWOANDTHREE_H
--- viewer.i
+++ viewer.i
@@ -1,9 +1,4 @@
 %module viewer
-%pythoncode %{
-        import six
-        if six.PY2:
-                PyBytes_FromStringAndSize= PyString_FromStringAndSize
-%}
 %typemap(out) bool&   %{ $result = PyBool_FromLong    ( *$1 ); %}
 %typemap(out) char&   %{ $result = PyInt_FromLong     ( *$1 ); %}
 %typemap(out) short&  %{ $result = PyInt_FromLong     ( *$1 ); %}
@@ -11,8 +6,12 @@
 %typemap(out) long&   %{ $result = PyInt_FromLong     ( *$1 ); %}
 %typemap(out) float&  %{ $result = PyFloat_FromDouble ( *$1 ); %}
 %typemap(out) double& %{ $result = PyFloat_FromDouble ( *$1 ); %}
+%{
+#include "2and3.h"
+%}
+%include "2and3.h"
 %typemap(out) PixelsData & %{ 
-        $result = PyBytes_FromStringAndSize((const char*)$1->get(), $1->get_size());
+        $result = MyPyBytes_FromStringAndSize((const char*)$1->get(), $1->get_size());
 %}
 %feature("autodoc","1");
 %include "carrays.i";
